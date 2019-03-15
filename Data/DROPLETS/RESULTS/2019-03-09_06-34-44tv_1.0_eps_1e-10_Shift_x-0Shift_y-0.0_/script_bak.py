Improvement of FFT in numpy/tensorflow - correct normalization

Data/DROPLETS/RESULTS/2019-03-09_06-34-44tv_1.0_eps_1e-10_Shift_x-0Shift_y-0.0_/script_bak.py
@@ -25,7 +25,7 @@ import src.data as data
 import src.tf_regularizers as reg
 
 # Optionally, tweak styles.
-mpl.rc('figure',  figsize=(9, 6))
+mpl.rc('figure',  figsize=(6, 4))
 mpl.rc('image', cmap='gray')
 #plt.switch_backend('agg')
 #np.set_printoptions(threshold=np.nan)
@@ -53,17 +53,17 @@ my_learningrate = 1e-1  # learning rate
 NreduceLR = 100 # when should we reduce the Learningrate? 
 
 lambda_tv = ((1e-0))##, 1e-2, 1e-2, 1e-3)) # lambda for Total variation - 1e-1
-eps_tv = ((1e-8))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
+eps_tv = ((1e-10))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
 # these are fixed parameters
 lambda_neg = 10000
-Niter = 1000
+Niter = 20000
 
 Noptpsf = 0
-Nsave = 100 # write info to disk
+Nsave = 1000 # write info to disk
 Ndisplay = Nsave
 # data files for parameters and measuremets 
-matlab_val_file = './Data/cells/Cell_20x_100a_150-250.tif_allAmp.mat'
-matlab_par_file = './Data/cells/Cell_20x_100a_150-250.tif_myParameter.mat'
+matlab_val_file = './Data/cells/Beads_40x_100a_100-250.tif_allAmp.mat'
+matlab_par_file = './Data/cells/Beads_40x_100a_100-250.tif_myParameter.mat'
 matlab_par_name = 'myParameter' 
 matlab_val_name = 'allAmpSimu' 
  
@@ -92,7 +92,7 @@ shiftIcX = 0*1 # has influence on the XZ-Plot - negative values shifts the input
 zernikefactors = 0*np.array((0,1,1,0,0,0,-.01,-.5001,0.01,0.01,.010))  # 7: ComaX, 8: ComaY, 11: Spherical Aberration
 #zernikefactors = 0*np.array(( 0., 0.001, 0.01, 0, 0, 0., -3.4e-03,  2.2e-03, 2.5e+00, 2.5e+00, -1.0e+00))
 
-zernikemask = np.array(np.abs(zernikefactors)>0)*1#!= np.array((0, 0, 0, 0, 0, 0, , 1, 1, 1, 1))# mask which factors should be updated
+zernikemask = np.ones(zernikefactors.shape) #np.array(np.abs(zernikefactors)>0)*1#!= np.array((0, 0, 0, 0, 0, 0, , 1, 1, 1, 1))# mask which factors should be updated
 
 '''START CODE'''
 tf.reset_default_graph() # just in case there was an open session
@@ -197,7 +197,7 @@ tf_loss = tf_fidelity + tf_tvloss + tf_negsqrloss
 '''Define Optimizer'''
 tf_optimizer = tf.train.AdamOptimizer(muscat.tf_learningrate)
 tf_lossop_norm = tf_optimizer.minimize(tf_loss, var_list = [tf_global_abs, tf_global_phase])
-tf_lossop_obj = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_obj, muscat.TF_obj_absorption])
+tf_lossop_obj = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_obj]) #  muscat.TF_obj_absorption
 tf_lossop_aberr = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_zernikefactors])
 tf_lossop = tf_optimizer.minimize(tf_loss)
 

Data/DROPLETS/RESULTS/2019-03-10_01-35-37tv_0.1_eps_1e-12_Shift_x-0Shift_y-0.0_/script_bak.py
@@ -25,7 +25,7 @@ import src.data as data
 import src.tf_regularizers as reg
 
 # Optionally, tweak styles.
-mpl.rc('figure',  figsize=(9, 6))
+mpl.rc('figure',  figsize=(6, 4))
 mpl.rc('image', cmap='gray')
 #plt.switch_backend('agg')
 #np.set_printoptions(threshold=np.nan)
@@ -52,18 +52,18 @@ NspikeLR = 25000 # try to get the system out of some local minima
 my_learningrate = 1e-1  # learning rate
 NreduceLR = 100 # when should we reduce the Learningrate? 
 
-lambda_tv = ((1e-0))##, 1e-2, 1e-2, 1e-3)) # lambda for Total variation - 1e-1
-eps_tv = ((1e-8))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
+lambda_tv = ((1e-1))##, 1e-2, 1e-2, 1e-3)) # lambda for Total variation - 1e-1
+eps_tv = ((1e-12))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
 # these are fixed parameters
 lambda_neg = 10000
 Niter = 1000
 
-Noptpsf = 0
+Noptpsf = 1
 Nsave = 100 # write info to disk
 Ndisplay = Nsave
 # data files for parameters and measuremets 
-matlab_val_file = './Data/cells/Cell_20x_100a_150-250.tif_allAmp.mat'
-matlab_par_file = './Data/cells/Cell_20x_100a_150-250.tif_myParameter.mat'
+matlab_val_file = './Data/cells/S0019-2a_zstack_dz0-02um_751planes_40x_every8thslice.tif_allAmp.mat'
+matlab_par_file = './Data/cells/S0019-2a_zstack_dz0-02um_751planes_40x_every8thslice.tif_myParameter.mat'
 matlab_par_name = 'myParameter' 
 matlab_val_name = 'allAmpSimu' 
  
@@ -89,8 +89,8 @@ if(0):
 NAc = .4
 shiftIcY = 0*.8 # has influence on the YZ-Plot - negative values shifts the input wave (coming from 0..end) to the left
 shiftIcX = 0*1 # has influence on the XZ-Plot - negative values shifts the input wave (coming from 0..end) to the left
-zernikefactors = 0*np.array((0,1,1,0,0,0,-.01,-.5001,0.01,0.01,.010))  # 7: ComaX, 8: ComaY, 11: Spherical Aberration
-#zernikefactors = 0*np.array(( 0., 0.001, 0.01, 0, 0, 0., -3.4e-03,  2.2e-03, 2.5e+00, 2.5e+00, -1.0e+00))
+zernikefactors = np.array((0,0,0,0,0,0,-.01,-.5001,0.01,0.01,.010))  # 7: ComaX, 8: ComaY, 11: Spherical Aberration
+zernikefactors = 0*np.array(( 0., 0.001, 0.01, 0, 0, 0., -3.4e-03,  2.2e-03, 2.5e+00, 2.5e+00, -1.0e+00))
 
 zernikemask = np.array(np.abs(zernikefactors)>0)*1#!= np.array((0, 0, 0, 0, 0, 0, , 1, 1, 1, 1))# mask which factors should be updated
 

Data/DROPLETS/RESULTS/2019-03-11_19-59-25tv_0.1_eps_1e-12_Shift_x-0Shift_y-0.0_/script_bak.py
@@ -25,7 +25,7 @@ import src.data as data
 import src.tf_regularizers as reg
 
 # Optionally, tweak styles.
-mpl.rc('figure',  figsize=(9, 6))
+mpl.rc('figure',  figsize=(6, 4))
 mpl.rc('image', cmap='gray')
 #plt.switch_backend('agg')
 #np.set_printoptions(threshold=np.nan)
@@ -52,25 +52,25 @@ NspikeLR = 25000 # try to get the system out of some local minima
 my_learningrate = 1e-1  # learning rate
 NreduceLR = 100 # when should we reduce the Learningrate? 
 
-lambda_tv = ((1e-0))##, 1e-2, 1e-2, 1e-3)) # lambda for Total variation - 1e-1
-eps_tv = ((1e-8))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
+lambda_tv = ((1e-1))##, 1e-2, 1e-2, 1e-3)) # lambda for Total variation - 1e-1
+eps_tv = ((1e-12))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
 # these are fixed parameters
 lambda_neg = 10000
-Niter = 1000
+Niter = 100
 
-Noptpsf = 0
-Nsave = 100 # write info to disk
+Noptpsf = 1
+Nsave = 10 # write info to disk
 Ndisplay = Nsave
 # data files for parameters and measuremets 
-matlab_val_file = './Data/cells/Cell_20x_100a_150-250.tif_allAmp.mat'
-matlab_par_file = './Data/cells/Cell_20x_100a_150-250.tif_myParameter.mat'
+matlab_val_file = './Data/cells/S0019-2a_zstack_dz0-02um_751planes_40x_every8thslice.tif_allAmp.mat'
+matlab_par_file = './Data/cells/S0019-2a_zstack_dz0-02um_751planes_40x_every8thslice.tif_myParameter.mat'
 matlab_par_name = 'myParameter' 
 matlab_val_name = 'allAmpSimu' 
  
 # need to figure out why this holds somehow true - at least produces reasonable results
 dn = .1
-myfac = dn*1e-3
-myabsnorm = myfac
+myfac = 0*1e2# 0*dn*1e-3
+myabsnorm = 1#myfac
 
 np_global_phase = 0.
 np_global_abs = 0.
@@ -89,8 +89,8 @@ if(0):
 NAc = .4
 shiftIcY = 0*.8 # has influence on the YZ-Plot - negative values shifts the input wave (coming from 0..end) to the left
 shiftIcX = 0*1 # has influence on the XZ-Plot - negative values shifts the input wave (coming from 0..end) to the left
-zernikefactors = 0*np.array((0,1,1,0,0,0,-.01,-.5001,0.01,0.01,.010))  # 7: ComaX, 8: ComaY, 11: Spherical Aberration
-#zernikefactors = 0*np.array(( 0., 0.001, 0.01, 0, 0, 0., -3.4e-03,  2.2e-03, 2.5e+00, 2.5e+00, -1.0e+00))
+zernikefactors = np.array((0,0,0,0,0,0,-.01,-.5001,0.01,0.01,.010))  # 7: ComaX, 8: ComaY, 11: Spherical Aberration
+zernikefactors = 0*np.array(( 0., 0.001, 0.01, 0, 0, 0., -3.4e-03,  2.2e-03, 2.5e+00, 2.5e+00, -1.0e+00))
 
 zernikemask = np.array(np.abs(zernikefactors)>0)*1#!= np.array((0, 0, 0, 0, 0, 0, , 1, 1, 1, 1))# mask which factors should be updated
 
@@ -142,7 +142,7 @@ sess.run(tf.global_variables_initializer())
 myATF = sess.run(muscat.TF_ATF)
 myASF = sess.run(muscat.TF_ASF) 
 ''' Define Fwd operator'''
-tf_fwd = muscat.computeconvolution(muscat.TF_ATF, myfac=myfac, myabsnorm = myabsnorm)
+tf_fwd = muscat.computeconvolution(muscat.TF_ASF, myfac=myfac, myabsnorm = myabsnorm)
 
 #%%
 ''' Compute a first guess based on the experimental phase '''
@@ -197,7 +197,7 @@ tf_loss = tf_fidelity + tf_tvloss + tf_negsqrloss
 '''Define Optimizer'''
 tf_optimizer = tf.train.AdamOptimizer(muscat.tf_learningrate)
 tf_lossop_norm = tf_optimizer.minimize(tf_loss, var_list = [tf_global_abs, tf_global_phase])
-tf_lossop_obj = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_obj, muscat.TF_obj_absorption])
+tf_lossop_obj = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_obj])#, muscat.TF_obj_absorption])
 tf_lossop_aberr = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_zernikefactors])
 tf_lossop = tf_optimizer.minimize(tf_loss)
 
@@ -313,9 +313,9 @@ for iterx in range(iter_last,Niter):
     # Alternate between pure object optimization and aberration recovery
     if (iterx>10) & (Noptpsf>0):
         for iterpsf in range(Noptpsf):
-           sess.run([tf_lossop_aberr], feed_dict={muscat.tf_meas:np_meas, muscat.tf_learningrate:my_learningrate, muscat.tf_lambda_tv:mylambdatv, muscat.tf_eps:myepstvval, muscat.TF_ATF_placeholder:myATF})
+           sess.run([tf_lossop_aberr], feed_dict={muscat.tf_meas:np_meas, muscat.tf_learningrate:my_learningrate, muscat.tf_lambda_tv:mylambdatv, muscat.tf_eps:myepstvval})
         for iterobj in range(Noptpsf):
-           sess.run([tf_lossop_obj], feed_dict={muscat.tf_meas:np_meas, muscat.tf_learningrate:my_learningrate, muscat.tf_lambda_tv:mylambdatv, muscat.tf_eps:myepstvval, muscat.TF_ATF_placeholder:myATF})
+           sess.run([tf_lossop_obj], feed_dict={muscat.tf_meas:np_meas, muscat.tf_learningrate:my_learningrate, muscat.tf_lambda_tv:mylambdatv, muscat.tf_eps:myepstvval})
     else:   
         sess.run([tf_lossop_obj], feed_dict={muscat.tf_meas:np_meas, muscat.tf_learningrate:my_learningrate, muscat.tf_lambda_tv:mylambdatv, muscat.tf_eps:myepstvval})
 

Listings_13_PSFmodel.py
@@ -19,7 +19,7 @@ import time
 # %matplotlib inline
 
 # Optionally, tweak styles.
-mpl.rc('figure',  figsize=(7, 4))
+mpl.rc('figure',  figsize=(9, 6))
 mpl.rc('image', cmap='gray')
 
 # load own functions
@@ -68,15 +68,15 @@ print('do we need to flip the data?! -> Observe FFT!!')
 ''' Create the Model'''
 muscat = mus.MuScatModel(matlab_pars, is_optimization=is_optimization)
 muscat.Nx,muscat.Ny = int(np.squeeze(matlab_pars['Nx'].value)), int(np.squeeze(matlab_pars['Ny'].value))
-zernikefactors = 0*np.array((0,0,0,0,0,0,-.1,2,0.01,0.01,.10)) # 7: ComaX, 8: ComaY, 11: Spherical Aberration
+zernikefactors = np.array((0,0,0,0,0,0,0,0,0.0,0.0,0.0)) # 7: ComaX, 8: ComaY, 11: Spherical Aberration
 zernikemask = np.array(np.abs(zernikefactors)>0)*1#!= np.array((0, 0, 0, 0, 0, 0, , 1, 1, 1, 1))# mask which factors should be updated
 muscat.shiftIcX = 0 # has influence on the XZ-Plot - negative values shifts the input wave (coming from 0..end) to the left
 muscat.shiftIcY = 0 # has influence on the YZ-Plot - negative values shifts the input wave (coming from 0..end) to the left
 muscat.NAc = .42
 
-dn = .1 #(1.437-1.3326)#/np.pi
-myfac = dn*1e-3#- 1e-6
-myabsnorm = myfac
+dn = (1+0j)/(2*np.pi/muscat.lambda0)**2 #(1.437-1.3326)#/np.pi
+myfac = 0*1e3*dn #- 1e-6
+myabsnorm = 1#myfac#*dn*1e1
 
 #muscat.NAo = .95
 #muscat.dz = 0.1625*2#muscat.lambda0/4
@@ -91,10 +91,10 @@ muscat.Nx = 32; muscat.Ny = 32; muscat.Nz = 70
 muscat.mysize = (muscat.Nz,muscat.Nx,muscat.Ny) # ordering is (Nillu, Nz, Nx, Ny)
 
 ''' Create a 3D Refractive Index Distributaton as a artificial sample'''
-mydiameter = 8
+mydiameter = 1
 if(1):
     obj = tf_go.generateObject(mysize=muscat.mysize, obj_dim=muscat.dx, obj_type ='sphere', diameter = mydiameter, dn = dn, nEmbb = muscat.nEmbb)#)dn)
-    obj_absorption = 0*tf_go.generateObject(mysize=muscat.mysize, obj_dim=muscat.dx, obj_type ='sphere', diameter = mydiameter, dn = .1, nEmbb = muscat.nEmbb)
+    obj_absorption = tf_go.generateObject(mysize=muscat.mysize, obj_dim=muscat.dx, obj_type ='sphere', diameter = mydiameter, dn = .0, nEmbb = muscat.nEmbb)
 elif(0):
     obj = tf_go.generateObject(mysize=muscat.mysize, obj_dim=muscat.dx, obj_type ='twosphere', diameter = mydiameter/8, dn = dn)#)dn)
     obj_absorption = tf_go.generateObject(mysize=muscat.mysize, obj_dim=muscat.dx, obj_type ='twosphere', diameter = mydiameter/8, dn = .01)
@@ -118,8 +118,11 @@ else:
     obj = np.load('./Data/PHANTOM/phantom_50_50_50.npy')*dn+ muscat.nEmbb
     obj_absorption = obj
         
-obj = muscat.nEmbb + dn*np.random.rand(muscat.Nz, muscat.Nx, muscat.Ny)
-obj = obj+1j*obj_absorption*0
+#import src.tf_helper as tf_helper
+obj = dn*(tf_helper.rr(obj.shape)<1)+1j*dn*(tf_helper.rr(obj.shape)<1)
+plt.imshow(np.real((2*np.pi/muscat.lambda0)**2*obj[:,:,16])), plt.colorbar(), plt.show()
+#obj = muscat.nEmbb + dn*np.random.rand(muscat.Nz, muscat.Nx, muscat.Ny)
+obj = obj+0*1j*obj_absorption
 ##obj = np.roll(obj,-9,0)
 
 
@@ -148,7 +151,7 @@ myASF = sess.run(muscat.TF_ASF)
 
 #%% run model and measure memory/time
 start = time.time()
-myfwd = sess.run(myres, feed_dict={muscat.TF_ATF_placeholder:myATF,  muscat.TF_obj:obj, muscat.TF_obj_absorption:obj_absorption})#, options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True), run_metadata=run_metadata)
+myfwd = sess.run(myres, feed_dict={muscat.TF_ASF_placeholder:myASF,  muscat.TF_obj:obj, muscat.TF_obj_absorption:obj_absorption})#, options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True), run_metadata=run_metadata)
 end = time.time()
 print(end - start)
 
@@ -158,32 +161,67 @@ print(end - start)
 centerslice = myfwd.shape[0]//2
 plt.figure()
 
-plt.subplot(231), plt.title('Angle XZ'),plt.imshow(np.angle(obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(232), plt.title('Angle YZ'),plt.imshow(np.angle(obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
-plt.subplot(233), plt.title('Angle XY'),plt.imshow(np.angle(obj)[myfwd.shape[0]//2,:,:]), plt.colorbar(), plt.show()
-plt.subplot(234), plt.title('ABS XZ'),plt.imshow(np.abs(obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(235), plt.title('ABS YZ'),plt.imshow(np.abs(obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
-plt.subplot(236), plt.title('ABS XY'),plt.imshow(np.abs(obj)[myfwd.shape[0]//2,:,:]), plt.colorbar(), plt.show()
+if(False):
+    #%%
+    plt.subplot(231), plt.title('real XZ'),plt.imshow(np.real(obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(232), plt.title('real YZ'),plt.imshow(np.real(obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
+    plt.subplot(233), plt.title('real XY'),plt.imshow(np.real(obj)[myfwd.shape[0]//2,:,:]), plt.colorbar(), plt.show()
+    plt.subplot(234), plt.title('imag XZ'),plt.imshow(np.imag(obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(235), plt.title('imag YZ'),plt.imshow(np.imag(obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
+    plt.subplot(236), plt.title('imag XY'),plt.imshow(np.imag(obj)[myfwd.shape[0]//2,:,:]), plt.colorbar(), plt.show()
+    
+    
+    plt.figure()    
+    plt.subplot(231), plt.imshow(np.abs(((myATF))**.2)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(232), plt.imshow(np.abs(((myATF))**.2)[myfwd.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+    plt.subplot(233), plt.imshow(np.abs(((myATF))**.2)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()    
+    plt.subplot(234), plt.imshow(np.abs(((myASF))**.2)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(235), plt.imshow(np.abs(((myASF))**.2)[myfwd.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+    plt.subplot(236), plt.imshow(np.abs(((myASF))**.2)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()    
 
-
-plt.figure()    
-plt.subplot(231), plt.imshow(np.abs(((myATF))**.2)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(232), plt.imshow(np.abs(((myATF))**.2)[myfwd.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
-plt.subplot(233), plt.imshow(np.abs(((myATF))**.2)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()    
-plt.subplot(234), plt.imshow(np.abs(((myASF))**.2)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(235), plt.imshow(np.abs(((myASF))**.2)[myfwd.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
-plt.subplot(236), plt.imshow(np.abs(((myASF))**.2)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()    
-
-myfwd_old = myfwd 
 #%%
+
+myfwd_old = myfwd
+#myfwd = np.fft.fftshift(myfwd) 
+#%%
+def abssqr(input):
+    return np.real(input*np.conj(input))
+
+myfwd = myfwd_old
 if(0):
-    myfwd = myfwd_old
+    mymean = np.mean(myfwd_old)
+    
+    myfwd = myfwd - 1e2j
     #myfac 
-    myfwd = (np.squeeze(myfwd_old)-(1e-4j))/(1e-4)
-    #myfac = 1e-4
+    myfwd = myfwd/(np.mean(np.abs(myfwd))) # (np.squeeze(myfwd_old)-(1e-4j))/(1e-4)
+    #myfac = 1e-4 
     #myfwd = (myfwd - 1j*myfac)/myfac
 #myfwd = (myfwd_old - 1j*myfac)/myfac
+  
+
+
+#%%
+
+plt.subplot(234), plt.imshow(np.imag(((myASF)))[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(235), plt.imshow(np.imag(((myASF)))[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+plt.subplot(236), plt.imshow(np.imag(((myASF)))[:,:,myASF.shape[2]//2]), plt.colorbar()#, plt.show()    
+plt.subplot(231), plt.imshow(np.real(((myASF)))[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(232), plt.imshow(np.real(((myASF)))[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+plt.subplot(233), plt.imshow(np.real(((myASF)))[:,:,myASF.shape[2]//2]), plt.colorbar(), plt.show()    
+
+plt.subplot(234), plt.imshow(np.imag(((myATF))**.2)[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(235), plt.imshow(np.imag(((myATF))**.2)[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+plt.subplot(236), plt.imshow(np.imag(((myATF))**.2)[:,:,myASF.shape[2]//2]), plt.colorbar()#, plt.show()    
+plt.subplot(231), plt.imshow(np.real(((myATF))**.2)[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(232), plt.imshow(np.real(((myATF))**.2)[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+plt.subplot(233), plt.imshow(np.real(((myATF))**.2)[:,:,myASF.shape[2]//2]), plt.colorbar(), plt.show()    
+
+
+
+
+#%%
 plt.figure()    
+myfwd = np.fft.fftshift(myfwd)
 plt.subplot(231), plt.title('real XZ'),plt.imshow(np.real(myfwd)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
 plt.subplot(232), plt.title('real YZ'),plt.imshow(np.real(myfwd)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
 plt.subplot(233), plt.title('real XY'),plt.imshow(np.real(myfwd)[centerslice ,:,:]), plt.colorbar()# plt.show()
@@ -192,25 +230,36 @@ plt.subplot(234), plt.title('imag XZ'),plt.imshow(np.imag(myfwd)[:,myfwd.shape[1
 plt.subplot(235), plt.title('imag YZ'),plt.imshow(np.imag(myfwd)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
 plt.subplot(236), plt.title('imag XY'),plt.imshow(np.imag(myfwd)[centerslice ,:,:]), plt.colorbar(), plt.show()
 
-#%%
 plt.figure()    
-plt.subplot(231), plt.title('muscat.obj Real XZ'),plt.imshow(np.real(muscat.obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(232), plt.title('muscat.obj Real XZ'),plt.imshow(np.real(muscat.obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
-plt.subplot(233), plt.title('muscat.obj Real XY'),plt.imshow(np.real(muscat.obj)[myfwd.shape[0]//2,:,:]), plt.colorbar()#, plt.show()
-plt.subplot(234), plt.title('muscat.obj Imag XZ'),plt.imshow(np.imag(muscat.obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(235), plt.title('muscat.obj Imag XZ'),plt.imshow(np.imag(muscat.obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
-plt.subplot(236), plt.title('muscat.obj Imag XY'),plt.imshow(np.imag(muscat.obj)[myfwd.shape[0]//2,:,:]), plt.colorbar(), plt.show()
-
-plt.figure()
-plt.subplot(231), plt.imshow(np.fft.fftshift(np.angle(sess.run(muscat.TF_Po_aberr))))
-plt.subplot(232), plt.imshow(np.fft.fftshift(np.abs(sess.run(muscat.TF_Po))))
-plt.subplot(233), plt.imshow((((muscat.Ic))))
-myObjFT = np.fft.fftshift(np.fft.fftn((myfwd)))
-plt.subplot(234), plt.imshow(np.abs(((myObjFT))**.12)[:,myATF.shape[1]//2,:]), plt.colorbar()#, plt.show()
-plt.subplot(235), plt.imshow(np.abs(((myObjFT))**.12)[myATF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
-plt.subplot(236), plt.imshow(np.abs(((myObjFT))**.12)[:,:,myATF.shape[2]//2]), plt.colorbar()#, plt.show()    
+plt.subplot(231), plt.title('abs XZ'),plt.imshow(np.abs(myfwd)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(232), plt.title('abs YZ'),plt.imshow(np.abs(myfwd)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
+plt.subplot(233), plt.title('abs XY'),plt.imshow(np.abs(myfwd)[centerslice ,:,:]), plt.colorbar()# plt.show()
+#myfwd=myfwd*np.exp(1j*2)
+plt.subplot(234), plt.title('angle XZ'),plt.imshow(np.angle(myfwd)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(235), plt.title('angle YZ'),plt.imshow(np.angle(myfwd)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
+plt.subplot(236), plt.title('angle XY'),plt.imshow(np.angle(myfwd)[centerslice ,:,:]), plt.colorbar(), plt.show()
 
 
+#%%
+if(False):
+    plt.figure()    
+    plt.subplot(231), plt.title('muscat.obj Real XZ'),plt.imshow(np.real(muscat.obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(232), plt.title('muscat.obj Real XZ'),plt.imshow(np.real(muscat.obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
+    plt.subplot(233), plt.title('muscat.obj Real XY'),plt.imshow(np.real(muscat.obj)[myfwd.shape[0]//2,:,:]), plt.colorbar()#, plt.show()
+    plt.subplot(234), plt.title('muscat.obj Imag XZ'),plt.imshow(np.imag(muscat.obj)[:,myfwd.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(235), plt.title('muscat.obj Imag XZ'),plt.imshow(np.imag(muscat.obj)[:,:,myfwd.shape[2]//2]), plt.colorbar()#, plt.show()
+    plt.subplot(236), plt.title('muscat.obj Imag XY'),plt.imshow(np.imag(muscat.obj)[myfwd.shape[0]//2,:,:]), plt.colorbar(), plt.show()
+    
+    plt.figure()
+    plt.subplot(231), plt.imshow(np.fft.fftshift(np.angle(sess.run(muscat.TF_Po_aberr))))
+    plt.subplot(232), plt.imshow(np.fft.fftshift(np.abs(sess.run(muscat.TF_Po))))
+    plt.subplot(233), plt.imshow((((muscat.Ic))))
+    myObjFT = np.fft.fftshift(np.fft.fftn((myfwd)))
+    plt.subplot(234), plt.imshow(np.abs(((myObjFT))**.12)[:,myATF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+    plt.subplot(235), plt.imshow(np.abs(((myObjFT))**.12)[myATF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+    plt.subplot(236), plt.imshow(np.abs(((myObjFT))**.12)[:,:,myATF.shape[2]//2]), plt.colorbar()#, plt.show()    
+    
+    
 #%% save the results
 np.save(savepath+'allAmp_simu.npy', myfwd)
 data.export_realdata_h5(filename = './Data/DROPLETS/allAmp_simu.mat', matname = 'allAmp_red', data=myfwd)

Listings_30_Reconstruction_BornFwd_Bead_40x_100a.py
@@ -56,7 +56,7 @@ lambda_tv = ((1e-0))##, 1e-2, 1e-2, 1e-3)) # lambda for Total variation - 1e-1
 eps_tv = ((1e-8))##, 1e-12, 1e-8, 1e-6)) # - 1e-1 # smaller == more blocky
 # these are fixed parameters
 lambda_neg = 10000
-Niter = 1000
+Niter = 1
 
 Noptpsf = 0
 Nsave = 100 # write info to disk
@@ -197,7 +197,7 @@ tf_loss = tf_fidelity + tf_tvloss + tf_negsqrloss
 '''Define Optimizer'''
 tf_optimizer = tf.train.AdamOptimizer(muscat.tf_learningrate)
 tf_lossop_norm = tf_optimizer.minimize(tf_loss, var_list = [tf_global_abs, tf_global_phase])
-tf_lossop_obj = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_obj]) #  muscat.TF_obj_absorption
+tf_lossop_obj = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_obj]) # 
 tf_lossop_aberr = tf_optimizer.minimize(tf_loss, var_list = [muscat.TF_zernikefactors])
 tf_lossop = tf_optimizer.minimize(tf_loss)
 
@@ -226,7 +226,17 @@ plt.subplot(232), plt.imshow(np.abs(((myATF))**.2)[myATF.shape[0]//2,:,:]), plt.
 plt.subplot(233), plt.imshow(np.abs(((myATF))**.2)[:,:,myATF.shape[2]//2]), plt.colorbar()#, plt.show()    
 plt.subplot(234), plt.imshow(np.abs(((myASF))**.2)[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
 plt.subplot(235), plt.imshow(np.abs(((myASF))**.2)[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
-plt.subplot(236), plt.imshow(np.abs(((myASF))**.2)[:,:,myASF.shape[2]//2]), plt.colorbar()#, plt.show()    
+plt.subplot(236), plt.imshow(np.abs(((myASF))**.2)[:,:,myASF.shape[2]//2]), plt.colorbar(), plt.show()    
+
+
+#%%
+plt.subplot(234), plt.imshow(np.imag(((myASF))**.2)[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(235), plt.imshow(np.imag(((myASF))**.2)[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+plt.subplot(236), plt.imshow(np.imag(((myASF))**.2)[:,:,myASF.shape[2]//2]), plt.colorbar()#, plt.show()    
+plt.subplot(231), plt.imshow(np.real(((myASF))**.2)[:,myASF.shape[1]//2,:]), plt.colorbar()#, plt.show()
+plt.subplot(232), plt.imshow(np.real(((myASF))**.2)[myASF.shape[0]//2,:,:]), plt.colorbar()#, plt.show()    
+plt.subplot(233), plt.imshow(np.real(((myASF))**.2)[:,:,myASF.shape[2]//2]), plt.colorbar(), plt.show()    
+
 
 #%%
     

src/model.py
@@ -309,7 +309,7 @@ class MuScatModel(object):
             self.myAllSlicePropagator_psf = 0*self.myAllSlicePropagator # dummy variable to make the algorithm happy
         
         if is_compute_psf=='sep':
-            self.Alldphi_psf = -(np.reshape(np.arange(0, self.mysize[0], 1), [1, 1, self.mysize[0]])-self.Nz/2)*np.repeat(np.fft.fftshift(self.dphi)[:, :, np.newaxis], self.mysize[0], axis=2)
+            self.Alldphi_psf = (np.reshape(np.arange(0, self.mysize[0], 1), [1, 1, self.mysize[0]])-self.Nz/2)*np.repeat(np.fft.fftshift(self.dphi)[:, :, np.newaxis], self.mysize[0], axis=2)
             self.myAllSlicePropagator_psf = np.transpose(np.exp(1j*self.Alldphi_psf) * (np.repeat(np.fft.fftshift(self.dphi)[:, :, np.newaxis], self.mysize[0], axis=2) >0), [2, 0, 1]);  # Propagates a single end result backwards to all slices
             #self.myAllSlicePropagator_psf = np.transpose(np.exp(1j*self.Alldphi_psf) * (np.repeat(self.dphi[:, :, np.newaxis], self.mysize[0], axis=2) >0), [2, 0, 1]);  # Propagates a single end result backwards to all slices
             
@@ -432,8 +432,7 @@ class MuScatModel(object):
           
         return TF_ASF, TF_ATF 
 
-
-    def computepsf(self):
+    def computepsf_old(self):
         # here we only want to gather the partially coherent transfer function PTF
         # which is the correlation of the PTF_c and PTF_i 
         # we compute it as the slice propagation of the pupil function
@@ -442,7 +441,7 @@ class MuScatModel(object):
         # Compute the ASF for the Condenser and Imaging Pupil
         self.TF_ASF_o = tf_helper.my_ift2d(self.TF_myAllSlicePropagator_psf * tf_helper.fftshift2d(self.TF_Po * self.TF_Po_aberr))
         self.TF_ASF_c = tf_helper.my_ift2d(self.TF_myAllSlicePropagator_psf * self.TF_Ic)
-        
+
         # 3.) correlation of the pupil function to get the APTF
         TF_ATF = tf_helper.my_ift3d(self.TF_ASF_o*tf.conj(self.TF_ASF_c))
         tf_global_phase = tf.angle(TF_ATF[self.mysize[0]//2,self.mysize[1]//2,self.mysize[2]//2])#tf.angle(self.TF_allAmp_3dft[0, self.mid3D[2], self.mid3D[1], self.mid3D[0]])
@@ -450,45 +449,101 @@ class MuScatModel(object):
         TF_ATF = TF_ATF * tf.exp(1j * tf_global_phase);  # normalize ATF
         
         # 4.) Comput the ATF and normalize it
-        TF_ASF = tf_helper.my_ft3d(TF_ATF)
+        TF_ASF = tf_helper.my_ift3d(TF_ATF)
         TF_ASF = TF_ASF/tf.cast(tf.sqrt(tf.reduce_sum(tf_helper.tf_abssqr(TF_ASF))), tf.complex64)
-        # self.TF_ATF = tf_helper.my_ift3d(self.TF_ASF)#/tf.cast(np.sqrt(np.prod(self.mysize)), tf.complex64)
+        #TF_ATF = tf_helper.my_ift3d(TF_ASF)#/tf.cast(np.sqrt(np.prod(self.mysize)), tf.complex64)
         return TF_ASF, TF_ATF 
 
-    def computeconvolution(self, TF_ATF=None, myfac=1e-3, myabsnorm=1):
+
+    def computepsf(self):
+        # here we only want to gather the partially coherent transfer function PTF
+        # which is the correlation of the PTF_c and PTF_i 
+        # we compute it as the slice propagation of the pupil function
+        
+        # 1. + 2.) Define the input field as the BFP and effective pupil plane of the condenser 
+        # Compute the ASF for the Condenser and Imaging Pupil
+        TF_ASF_po = tf_helper.my_ift2d(self.TF_myAllSlicePropagator_psf * tf_helper.fftshift2d(self.TF_Po ))#* self.TF_Po_aberr))
+        TF_ASF_ic = tf_helper.my_ift2d(self.TF_myAllSlicePropagator_psf * self.TF_Ic)
+        #TF_ASF_po = TF_ASF_po/tf.complex(np.float32(np.sqrt(np.prod(self.mysize))),0.); # not necerssary for TF!
+        #TF_ASF_ic = TF_ASF_ic/tf.complex(np.float32(np.sqrt(np.prod(self.mysize))),0.); # not necerssary for TF!
+        
+        # normalize ATF to their maximum maginitude 
+        TF_ATF_po = tf_helper.my_ft3d(TF_ASF_po)
+        TF_ATF_po = TF_ATF_po/tf.complex(tf.reduce_max(tf.abs(TF_ATF_po)),0.)
+        TF_ATF_ic = tf_helper.my_ft3d(TF_ASF_ic) 
+        TF_ATF_ic = TF_ATF_ic/tf.complex(tf.reduce_max(tf.abs(TF_ATF_ic)),0.)
+
+        sess = tf.Session();         sess.run(tf.global_variables_initializer())
+        sess.run(TF_ATF_po)
+        #myV = sess.run(self.TF_V)
+        #plt.imshow(np.abs(sess.run(tf_helper.my_ft3d(self.TF_V)))[:,16,:]), plt.colorbar(), plt.show()
+        #plt.imshow(np.angle(sess.run(tf_helper.my_ft3d(self.TF_V)))[:,16,:]), plt.colorbar(), plt.show()        
+        
+        # 3.) correlation of the pupil function to get the APTF
+        TF_ASF = tf_helper.my_ft3d(TF_ATF_po)*tf.conj(tf_helper.my_ft3d(TF_ATF_ic))
+        TF_ASF = TF_ASF/tf.complex(np.float32(np.sqrt(np.prod(self.mysize))),0.); # not necerssary for TF!
+        TF_ATF = tf_helper.my_ft3d(TF_ASF)
+        TF_ATF = TF_ATF/tf.complex(np.float32(np.sqrt(np.prod(self.mysize))),0.); # not necerssary for TF!
+        myTF_ATF = sess.run(TF_ATF)
+        
+        #plt.imshow(np.sum(np.abs(myTF_ATF),0)), plt.colorbar(), plt.show()
+        #plt.imshow(np.angle(sess.run(tf_helper.my_ft3d(self.TF_V)))[:,16,:]), plt.colorbar(), plt.show()        
+        
+        #
+        
+        # Following is the normalization according to Martin's book. We do not use it, because the
+        # following leads to divide by zero for dark-field system.
+        normfactor = np.abs(np.fft.ifftshift(self.Po)**2)*np.abs(self.Ic); 
+        normfactor = np.sum(normfactor)
+        print(normfactor)
+        # %If normafactor == 0, we are imaging with dark-field system.
+        TF_ATF = TF_ATF/tf.complex(np.float32(normfactor*self.lambda0*4.*np.pi),0.)
+        TF_ASF = tf_helper.my_ift3d(TF_ATF)
+        #myasf = myasf/sqrt(sum(abssqr(myasf))); % not necerssary for TF!
+
+        return TF_ASF, TF_ATF 
+
+    def computeconvolution(self, TF_ASF=None, myfac=1., myabsnorm=1):
         # We want to compute the born-fwd model
         # TF_ATF - is the tensorflow node holding the ATF - alternatively use numpy arry!
         print('Computing the fwd model in born approximation')
-        
-        TF_real_3D = self.TF_obj
-        TF_imag_3D = self.TF_obj_absorption    
-        
-        # wrapper for force-positivity on the RI-instead of penalizing it
-        if(self.is_forcepos):
-            print('----> ATTENTION: We add the PreMonotonicPos' )
-            TF_real_3D = tf_reg.PreMonotonicPos(TF_real_3D)
-            TF_imag_3D = tf_reg.PreMonotonicPos(TF_imag_3D)
 
-        TF_f = tf.complex(TF_real_3D, TF_imag_3D)
-        TF_V = (2*np.pi/self.lambda0)**2*(TF_f**2-self.nEmbb**2)
-
+        mysample = (tf_helper.rr(self.obj.shape)<1)*.1
+        mysample = 1.33+mysample + 0j
+        k02 = (2*np.pi/.65)**2
+        myV = (k02*(mysample**2-1.33**2))
+        if(1):
+            # compute the scattering potential according to 1st Born
+            self.TF_nr = tf.complex(self.TF_obj, self.TF_obj_absorption)
+            self.TF_no = tf.cast(self.nEmbb+0j, tf.complex64)
+            #k02 = (2*np.pi/self.lambda0)**2
+            self.TF_V = k02*(self.TF_nr**2-self.TF_no**2)
+        #self.TF_V = tf.cast(myV,tf.complex64)
+        
+        
         # We need to have a placeholder because the ATF is computed afterwards...
-        if (TF_ATF is None):
-            self.TF_ATF_placeholder = tf.placeholder(dtype=tf.complex64, shape=self.mysize, name='TF_ATF_placeholder')
+        if (TF_ASF is None):
+            self.TF_ASF_placeholder = tf.placeholder(dtype=tf.complex64, shape=self.mysize, name='TF_ASF_placeholder')
         else:
-            self.TF_ATF_placeholder = TF_ATF
-                
+            self.TF_ASF_placeholder = tf.cast(TF_ASF, tf.complex64)
+        
+        
+        #sess = tf.Session()
+        #sess.run(tf.global_variables_initializer())
+        #myV = sess.run(self.TF_V)
+        #plt.imshow(np.abs(sess.run(tf_helper.my_ft3d(self.TF_V)))[:,16,:]), plt.colorbar(), plt.show()
+        #plt.imshow(np.angle(sess.run(tf_helper.my_ft3d(self.TF_V)))[:,16,:]), plt.colorbar(), plt.show()        
 
         # convolve object with ASF
-        TF_res = (tf_helper.my_ift3d(tf_helper.my_ft3d(TF_V)*self.TF_ATF_placeholder))
-        print('ATTENTION: WEIRD MAGIC NUMBER for background field!!')
-        #myfac=1e-3 # why and which factor makes sense?
-        print(myfac)
+        #TF_res = tf_helper(tf.ifft3d(tf.fft3d(self.TF_V)*tf.fft3d(self.TF_ASF_placeholder)))
+        myfftfac = np.prod(self.mysize)
         
-        #TF_res = (TF_res-(1j*(1e-4)))/(1e-4)# tf.complex64) #-tf.complex(0., myfac))/tf.complex(myabsnorm,0.)
-        #TF_res = TF_res-tf.cast(tf.constant(1j*1e-4), tf.complex64)
+        #TF_V = tf.cast(myV,tf.complex64)
+        TF_res = tf_helper.my_ift3d(tf_helper.my_ft3d(self.TF_V,myfftfac)*tf_helper.my_ft3d(self.TF_ASF_placeholder,myfftfac),myfftfac)
+        print('ATTENTION: WEIRD MAGIC NUMBER for background field!!')
+        
         #return (tf.squeeze(TF_res)-1e-4j)/1e-4#tf.squeeze((TF_res-(0+1j*(1e-4)))/(1e-4))
-        return (tf.squeeze(TF_res)-1j*myfac)/myabsnorm #tf.squeeze((TF_res-(0+1j*(1e-4)))/(1e-4))
+        return tf.squeeze(TF_res-1j*myfac)/myfac 
            
         
     def propslices(self):

src/tf_generate_object.py
@@ -106,7 +106,7 @@ def generateObject(mysize = [100, 100, 100], obj_dim = [0.1, 0.1, 0.1], obj_type
         obj = mysample + nEmbb
         #plt.imshow(mysample[:,:,16])
 
-    return obj
+    return np.float32(obj)
 
 
             

src/tf_helper.py
@@ -158,7 +158,7 @@ def ifftshift3d(tensor):
 
 
 # I would recommend to use this
-def my_ft2d(tensor):
+def my_ft2d(tensor, scaling=1.):
     """
     fftshift(fft(ifftshift(a)))
     
@@ -167,9 +167,9 @@ def my_ft2d(tensor):
     
     Uses standard normalization of fft unlike dip_image.
     """
-    return fftshift2d(tf.fft2d(ifftshift2d(tensor)))
+    return fftshift(tf.fft2d(ifftshift(tensor,2)),2)/scaling
 
-def my_ift2d(tensor):
+def my_ift2d(tensor, scaling=1.):
     """
     fftshift(ifft(ifftshift(a)))
     
@@ -178,9 +178,9 @@ def my_ift2d(tensor):
     
     Uses standard normalization of ifft unlike dip_image.
     """
-    return fftshift2d(tf.ifft2d(ifftshift2d(tensor)))
+    return fftshift(tf.ifft2d(ifftshift(tensor)))*scaling
 
-def my_ft3d(tensor):
+def my_ft3d(tensor, scaling=1.):
     """
     fftshift(fft(ifftshift(a)))
     
@@ -189,9 +189,9 @@ def my_ft3d(tensor):
     
     Uses standard normalization of fft unlike dip_image.
     """
-    return fftshift3d(tf.fft3d(ifftshift3d(tensor)))
+    return fftshift(tf.fft3d(ifftshift(tensor)))/scaling
 
-def my_ift3d(tensor):
+def my_ift3d(tensor, scaling=1.):
     """
     fftshift(ifft(ifftshift(a)))
     
@@ -200,9 +200,62 @@ def my_ift3d(tensor):
     
     Uses standard normalization of fft unlike dip_image.
     """
-    return fftshift3d(tf.ifft3d(ifftshift3d(tensor)))
+    return fftshift(tf.ifft3d(ifftshift(tensor)))*scaling
 
+def fftshift(tfin):
+    '''
+    shifts the coordinate space before an FFT.
 
+    performs an fftshift operation, cyclicly wrapping around by a shift vector corresponding to the middle.
+    The middle will end up at the zero coordinate pixel.
+
+    Parameters
+    ----------
+    tfin : tensorflow array to be shifted
+
+    Returns
+    -------
+    shifted tensorflow array
+    
+    '''
+    with tf.name_scope('preFFTShift'):
+        return tf.manip.roll(tfin,shift=-MidPos(tfin),axis=tf.range(0,tf.size(tf.shape(tfin))))  # makes a copy which is shifted
+
+def ifftshift(tfin):
+    '''
+    shifts the coordinate space after an FFT.
+
+    performs an fftshift operation, cyclicly wrapping around by a shift vector corresponding to the middle.
+    The middle will end up at the zero coordinate pixel.
+
+    Parameters
+    ----------
+    tfin : tensorflow array to be shifted
+
+    Returns
+    -------
+    shifted tensorflow array
+    
+    '''
+    with tf.name_scope('postFFTShift'):
+        return tf.manip.roll(tfin,shift=MidPos(tfin),axis=tf.range(0,tf.size(tf.shape(tfin))))  # makes a copy which is shifted
+
+def MidPos(tfin):
+    '''
+    helper function to get the mid-point in integer coordinates of tensorflow arrays.
+
+    It calculates the floor of the integer division of the shape vector. This is useful to get the zero coordinate in Fourier space
+
+    Parameters
+    ----------
+    tfin : tensorflow array to be convolved with the PSF
+
+    Returns
+    -------
+    vector to mid point 
+    
+    '''
+    return tf.floordiv(tf.shape(tfin),2)
 
 
 ###### CHRISTIANS STUFF